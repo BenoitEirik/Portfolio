c66bc870c2df15d4a934783e15a8ad1366f078c1
BenoitEirik <olafsergent@gmail.com>
2024-10-24 01:11:58 +0200

    Add Borda fonts

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex min-h-screen w-screen max-w-full items-center justify-between bg-gray-900 px-20 py-4 text-white [background:_url(/Division-2-rues.jpg)_center/cover]">
+  <div class="flex min-h-screen w-screen max-w-full items-center justify-between bg-gray-900 px-20 py-4 font-[Borda] font-bold text-white [background:_url(/Division-2-rues.jpg)_center/cover]">
     <div class="h-10 w-10"></div>
     <NuxtPage class="h-[1040px] max-h-[1040px] w-[1880px] max-w-[1880px] backdrop-blur-sm [perspective-origin:_right] [transform:_perspective(800px)_rotateY(-3deg)_rotateX(-0.5deg)]" />
   </div>
