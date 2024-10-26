fcc11cb587e5d50e9110948c11861e8e6cbbda6c
BenoitEirik <olafsergent@gmail.com>
2024-10-26 16:53:03 +0200

    Add functional tab

---
+++ pages/portfolio/competences-humaines.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    Compétences humaines
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
---
+++ pages/portfolio/competences-techniques.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    Compétences techniques
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
