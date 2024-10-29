3b713635d5cf9288f46d1a818de209596fcb4aff
BenoitEirik <olafsergent@gmail.com>
2024-10-29 15:42:18 +0100

    Add input component

---
+++ components/Input.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+const props = defineProps<{
+  type: string,
+  iconName?: string
+}>()
+</script>
+
+<template>
+  <div class="flex bg-neutral-600/50 transition active:bg-amber-600/50 active:[transform:scale(0.975)]">
+    <input :type="props.type"
+      class="h-12 grow border-b-4 border-neutral-300/20 bg-transparent px-3 outline-none transition focus:border-b-amber-600 [&+button]:focus:border-b-amber-600">
+    <button
+      class="outline-none active:border-b-amber-600 relative flex shrink-0 items-center justify-items-center border-b-4 border-neutral-300/20 px-3 [&>span]:hover:scale-110 transition before:absolute before:left-0 before:h-[calc(100%-15px)] before:w-[1px] before:bg-neutral-300/20">
+      <Icon v-if="props.iconName" :name="props.iconName" style="color: white" size="24px" />
+    </button>
+  </div>
+</template>
+
+<style></style>
